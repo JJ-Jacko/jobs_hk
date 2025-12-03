09b7d4a2bee963136503544fdafd4a4696ddf629
Jacko <l2982211930@163.com>
2025-12-03 15:46:31 +0800

    feat(libs/filter/JobCardFilter): 提取 JobCard 页面的每个工作的信息

--- src/jobs_hk/libs/filter.py
+++ src/jobs_hk/libs/filter.py
@@ -1,10 +1,18 @@
 import re
 from urllib import parse
 from typing import List
+from datetime import datetime
 
 from bs4 import BeautifulSoup
 
 from jobs_hk.libs.datas import Job
+
+
+def fill_none(content: str):
+    if content in ["-"]:
+        return None
+    else:
+        content
 
 
 class JobSearchFilter:
@@ -47,3 +55,40 @@ class JobSearchFilter:
             ))
         
         return jobs
+    
+    
+class JobCardFilter:
+    soup: BeautifulSoup
+    
+    def __init__(self, html_content: str | bytes):
+        self.soup = BeautifulSoup(html_content, "html.parser")
+
+    def get_job_info(self):
+        dt_formate = "%d/%m/%Y"
+        
+        num = int(self.soup.find("span", id="noVac").get_text(strip=True))
+        post_date = datetime.strptime(
+            self.soup.find("span", id="postedDt").get_text(strip=True),
+            dt_formate
+        ).date()
+        company_name = self.soup.find("span", id="empName").get_text(strip=True)
+        location = self.soup.find("span", id="locDesc").get_text(strip=True)
+        industry = self.soup.find("span", id="indsDesc").get_text(strip=True)
+        job_remark = self.soup.find("span", id="jobRemark").get_text(strip=True)
+        edu_remark = self.soup.find("span", id="eduRemark").get_text(strip=True)
+        compensation = self.soup.find("span", id="empTerm").get_text(strip=True)
+        openup_remark = self.soup.find("span", id="openupRemark").get_text(strip=True)
+        prop_remark = fill_none(self.soup.find("span", id="propRemark").get_text(strip=True))
+        
+        return {
+            "num": num,
+            "post_date": post_date,
+            "company_name": company_name,
+            "location": location,
+            "industry": industry,
+            "job_remark": job_remark,
+            "edu_remark": edu_remark,
+            "compensation": compensation,
+            "openup_remark": openup_remark,
+            "prop_remark": prop_remark
+        }
