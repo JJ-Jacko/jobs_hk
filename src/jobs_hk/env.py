1fdc3ba3009b1b186756cd140abd7bd04c62852e
Jacko <l2982211930@163.com>
2026-06-18 23:00:19 +0800

    feat(env.get_ddl_text)

--- src/jobs_hk/env.py
+++ src/jobs_hk/env.py
@@ -1,5 +1,8 @@
+import sqlalchemy.dialects as dialects
 from sqlalchemy.engine import Engine
+from sqlalchemy.schema import CreateTable
 from sqlmodel import create_engine
+from sqlmodel import SQLModel
 
 from jobs_hk.datas import Job
 from jobs_hk.datas import Company
@@ -15,3 +18,13 @@ def init_database(engine: Engine):
     Job.metadata.create_all(engine)
     Company.metadata.create_all(engine)
     Contact.metadata.create_all(engine)
+
+
+def get_ddl_text():
+    ddl_text = ""
+    
+    for table in SQLModel.metadata.sorted_tables:
+        sql_compiled = CreateTable(table).compile(dialect=dialects.sqlite.dialect())
+        ddl_text += f"{str(sql_compiled).strip()};\n\n"
+    
+    return ddl_text
