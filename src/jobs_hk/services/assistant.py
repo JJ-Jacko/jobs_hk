be6bdc7b961ca8a852eef4c4358d6915752224cb
Jacko <l2982211930@163.com>
2026-07-05 20:30:27 +0800

    refactor(cli.ask.Ask -> services.Assistant)

---
+++ src/jobs_hk/services/assistant.py
@@ -0,0 +1,211 @@
+from typing import Dict
+
+import ollama
+
+from jobs_hk.cli import context
+from jobs_hk.env import get_ddl_text
+from jobs_hk.exceptions import ModelGenerateException
+from jobs_hk.exceptions import SQLStatementExecException
+from jobs_hk.other import keywords_in_text
+from jobs_hk.schemas import SQLGen
+
+
+__all__ = ["Assistant"]
+
+
+class Assistant:
+    client: ollama.Client
+    tools: Dict[str, function]
+
+    def __init__(self, host: str):
+        self.client = ollama.Client(host)
+        self.tools = {
+            "get_jobs_basic_info": context.db.get_jobs_basic_info,
+            "query_jobs_database": self.query_jobs_database
+        }
+
+    def chat(self, user_prompt: str):
+        system_prompt = context.project_assistant_p.read_text()
+        
+        messages = [
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
+                "role": "user",
+                "content": user_prompt
+            }
+        ]
+        
+        resp = self.client.chat(
+            model=context.project_config["ollama"]["chat_model"],
+            messages=messages,
+            think=False,
+            tools=[
+                context.db.get_jobs_basic_info,
+                self.query_jobs_database
+            ]
+        )
+        
+        if resp.message.tool_calls:
+            messages.append(resp.message)
+            for call in resp.message.tool_calls:
+                func = self.tools[call.function.name]
+                res = func(**call.function.arguments)
+                messages.append({
+                    "role": "tool",
+                    "tool_name": call.function.name,
+                    "content": str(res)
+                })
+            
+            resp = self.client.chat(
+                model=context.project_config["ollama"]["chat_model"],
+                messages=messages,
+                think=False,
+                options={
+                    "num_ctx": 16 * 1024,
+                }
+            )
+        
+        return resp.message.content
+
+    def generate_sql(self, user_prompt: str) -> str:
+        """Generates a SQLite statement from a direct natural language query.
+
+        Args:
+            user_prompt:
+                A direct and explicit description of the data query NOT the SQL statement.
+                (e.g., "Find jobs with salary upper than 5000"). 
+                - CRITICAL: Do NOT use vague or conversational phrasing, or the model will fail.
+        """
+        
+        ddl_text = get_ddl_text()
+        system_prompt_template = context.sql_generator_sqlite_only_p.read_text()
+        system_prompt = system_prompt_template.format(ddl_text=ddl_text)
+        
+        messages = [
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
+                "role": "user",
+                "content": user_prompt
+            }
+        ]
+        
+        while True:
+            resp = self.client.chat(
+                model=context.project_config["ollama"]["code_model"],
+                messages=messages,
+                format=SQLGen.model_json_schema()
+            )
+            
+            sql_gen = SQLGen.model_validate_json(resp.message.content)
+            messages.append(resp.message)
+            
+            # Check status in rule
+            if sql_gen.status not in ["REJECT", "RESPOND_FAIL", "RESPOND_SUCCESS"]:
+                messages.append({
+                    "role": "user",
+                    "content": "Schema violation: `status` MUST fill with REJECT, RESPOND_FAIL and RESPOND_SUCCESS"
+                })
+                continue
+            
+            if sql_gen.status == "REJECT":
+                if sql_gen.sql is not None and sql_gen.error_message is not None:
+                    messages.append({
+                        "role": "user",
+                        "content": (
+                            "Schema violation: If `status` is REJECT, "
+                            "fill `sql`, `error_message` both with null."
+                        )
+                    })
+                    continue
+                
+                raise ModelGenerateException(
+                    type="REJECT",
+                    model_content=resp.message.content,
+                    message=(
+                        "The request is invalid, malicious, non-query or irrelevant conversational text "
+                        "or tries to INSERT/UPDATE/DELETE/DROP these illegal modification."
+                    )
+                )
+            
+            if sql_gen.status == "RESPOND_FAIL":
+                if sql_gen.sql is not None or sql_gen.error_message is None:
+                    messages.append({
+                        "role": "user",
+                        "content": (
+                            "Schema violation: If `status` is RESPOND_FAIL, "
+                            "fill `sql` with null, "
+                            "fill `error_message` with the spcific reason."
+                        )
+                    })
+                    continue
+                
+                raise ModelGenerateException(
+                    type="RESPOND_FAIL",
+                    model_content=resp.message.content,
+                    message="The request is a query intent but cannot be fulfilled due to missing data/tables."
+                )
+            
+            # Check sql statement security
+            if (
+                "SELECT" not in sql_gen.sql
+                or keywords_in_text(["INSERT", "UPDATE", "DELETE", "DROP"], sql_gen.sql)
+            ):
+                messages.append({
+                    "role": "user",
+                    "content": (
+                        "Security/Policy violation: The generated SQL must be a Read-Only 'SELECT' query. "
+                        "Do NOT use destructive or modification keywords (INSERT, UPDATE, DELETE, DROP)."
+                    )
+                })
+                continue
+            
+            return sql_gen.sql
+
+    def query_jobs_database(self, user_prompt: str):
+        """
+        Query the database using a natural language descripion.
+        
+        Convert the request into a SQL SELECT statement and executes it,
+        returning matching records or an error description
+        if the query cound not be fulfilled.
+
+        Args:
+            user_prompt:
+                A direct and explicit description of the data query NOT the SQL statement.
+                (e.g., "Find jobs with salary upper than 5000"). 
+                - CRITICAL: Do NOT use vague or conversational phrasing, or the model will fail.
+        """
+        
+        try:
+            statement = self.generate_sql(user_prompt)
+        except ModelGenerateException as e:
+            return (
+                "Query execution FAILED.\n"
+                f"Reason: {str(e)}\n"
+                "Attention: Do NOT expose raw SQL or technical error datails to the user, "
+                "instead, explain in natural language that the request could not be fulfilled "
+                "and suggest the user or simplify their query."
+            )
+        
+        try:
+            info = context.db.get_jobs_specific_info(statement)
+        except SQLStatementExecException as e:
+            return (
+                "Query execution FAILED.\n"
+                f"Reason: {str(e)}\n"
+                "Attention: Do NOT expose raw SQL or technical error datails to the user, "
+                "instead, explain in natural language that the request could not be fulfilled "
+                "and suggest the user or simplify their query."
+            )
+        else:
+            return (
+                "Query execution SUCCESSED.\n"
+                f"Result: {str(info)}"
+            )
+        
