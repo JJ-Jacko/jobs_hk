15765f8a77f42d428e68802d29d94f4962520969
Jacko <l2982211930@163.com>
2026-07-25 13:26:52 +0800

    feat(server): Add tool `query_jobs_database`.

--- src/jobs_hk/server.py
+++ src/jobs_hk/server.py
@@ -2,6 +2,7 @@ from mcp.server.fastmcp import FastMCP
 
 import jobs_hk.context as context
 from jobs_hk.db import DB
+from jobs_hk.services.assistant import Assistant
 
 
 mcp = FastMCP("Jobs HongKong")
@@ -9,7 +10,12 @@ mcp = FastMCP("Jobs HongKong")
 
 if __name__ == "__main__":
     db = DB(context.engine)
+    service = Assistant(
+        host=context.project_config["ollama"]["host"],
+        db=db
+    )
     mcp.add_tool(db.get_jobs_basic_info)
+    mcp.add_tool(service.query_jobs_database)
     
     mcp.run(transport="stdio")
     
